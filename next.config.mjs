0b75ab09537e2dbe80ac283fd4f5fba27b905e78
Martinix3 <maritnjaime85@gmail.com>
2025-09-16 18:16:39 +0000

    eckte habias grabado a fuego package.json verdad?

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: [
-      'https://3000-firebase-studio-1757248254463.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev'
-    ],
+  webpack(config) {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    return config;
   },
 };
 
