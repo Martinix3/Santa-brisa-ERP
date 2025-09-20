0f2d87fe40cc43bc64fcc746fc170f049363a727
Martinix3 <maritnjaime85@gmail.com>
2025-09-20 07:20:42 +0000

    ese bug no viene de ahí

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: "true",
   experimental: {},
   allowedDevOrigins: [
     'https://3000-firebase-studio-1757248254463.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev',
