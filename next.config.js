0b429a31e04fed4f098ede1203a7ee81183280df
Martinix3 <maritnjaime85@gmail.com>
2025-09-20 06:36:35 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   allowedDevOrigins: [
     'https://3000-firebase-studio-1757248254463.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev',
     'https://9000-firebase-studio-1757248254463.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev',
-    'http://localhost:3000',
-    'http://10.88.0.3:3000',
+    'http://localhost:3001',
+    'http://10.88.0.3:3001',
   ],
   async redirects() {
     return [{ source: '/signup', destination: '/login', permanent: true }];
