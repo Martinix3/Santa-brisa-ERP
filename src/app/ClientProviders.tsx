2d7dbdb8bb70620e8a16f583e5473fe16d59f49b
Martinix3 <maritnjaime85@gmail.com>
2025-09-30 20:44:42 +0000

    bundle.js:45 [Fast Refresh] rebuilding bundle.js:45 [DataProvider] Attem

--- src/app/ClientProviders.tsx
+++ src/app/ClientProviders.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import { DataProvider } from "@/lib/dataprovider";
+import { RealtimeProvider } from "@/app/providers/RealtimeProvider";
 
 export default function ClientProviders({
   children,
@@ -9,8 +10,10 @@ export default function ClientProviders({
   children: React.ReactNode;
 }) {
   return (
+    <RealtimeProvider>
       <DataProvider>
           {children}
       </DataProvider>
+    </RealtimeProvider>
   );
 }
