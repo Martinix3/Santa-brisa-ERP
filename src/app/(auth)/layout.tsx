d4410cedff7dd9f681bdf515bc3c51911db7ac0f
Martinix3 <maritnjaime85@gmail.com>
2025-10-04 08:52:48 +0000

    ✓ Ready in 23s  ○ Compiling / ...  ✓ Compiled / in 40.8s (890 modules)

--- src/app/(auth)/layout.tsx
+++
@@ -1,2 +0,0 @@
-// This layout is no longer needed as the auth routes are handled differently.
-// This file will be deleted.
--- src/app/(auth)/login/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is being moved. Its contents are now in src/app/login/page.tsx.
-// This file will be deleted.
